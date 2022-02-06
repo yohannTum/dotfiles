1e889ddf6e7beb7fdf3d0973eb590c088fd1ae15
yohann <yohann.tumpling@gmail.com>
2022-02-06 19:45:36 +0100

    adding i3 and polybar config

---
+++ polybar/launch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+
+
+/opt/polybar/build/bin/polybar
+
+# Launch bar1 and bar2
+# echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+# /opt/polybar/build/bin/polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+# /opt/polybar/build/bin/polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+# 
+# echo "Bars launched..."
