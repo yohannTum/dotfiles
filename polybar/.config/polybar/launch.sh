ee9286edba96e2dd5c883c43329b809089cf1e6e
yohann <yohann.tumpling@gmail.com>
2022-02-17 19:45:11 +0100

    polybar update

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,10 +6,13 @@ killall -q polybar
 # polybar-msg cmd quit
 
 
-/opt/polybar/build/bin/polybar
+# /opt/polybar/build/bin/polybar
 
 # Launch bar1 and bar2
 # echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+/opt/polybar/build/bin/polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+sleep 1
+/opt/polybar/build/bin/polybar bar2 2>&1 | tee -a /tmp/polybar1.log & disown
 # /opt/polybar/build/bin/polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
 # /opt/polybar/build/bin/polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
 # 
