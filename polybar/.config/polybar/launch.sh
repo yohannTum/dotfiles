688afed5e56d165c72ba6050cab7128fe5a2dd85
yohann <yohann.tumpling@gmail.com>
2024-01-12 13:09:58 +0100

    feat(polybar): add backlight support, launcher, power menu and double monitor support

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -10,10 +10,31 @@ killall -q polybar
 
 # Launch bar1 and bar2
 # echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-/opt/polybar/build/bin/polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
-sleep 1
-/opt/polybar/build/bin/polybar bar2 2>&1 | tee -a /tmp/polybar1.log & disown
+
+
+# ONE SCREEN
+# /opt/polybar/build/bin/polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+# sleep 1
+# /opt/polybar/build/bin/polybar bar2 2>&1 | tee -a /tmp/polybar1.log & disown
+
+
 # /opt/polybar/build/bin/polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
 # /opt/polybar/build/bin/polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
 # 
 # echo "Bars launched..."
+
+# EVERY SCREENS
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m /opt/polybar/build/bin/polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+        sleep 1
+        MONITOR=$m /opt/polybar/build/bin/polybar bar2 2>&1 | tee -a /tmp/polybar1.log & disown
+        # MONITOR=$m polybar --reload example &
+    done
+else
+    # ONE SCREEN
+    /opt/polybar/build/bin/polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+    sleep 1
+    /opt/polybar/build/bin/polybar bar2 2>&1 | tee -a /tmp/polybar1.log & disown
+    # polybar --reload example &
+fi
